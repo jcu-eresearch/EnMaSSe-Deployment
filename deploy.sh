3c2a7c0ce129b11d338b494b549efdf89fc014ba
Casey Bajema <casey@bajtech.com.au>
2013-04-29 14:01:21 +1000

    Removed the deform source checkout as it shouldnt be needed anymore (was required for a minor file upload fix).

--- deploy.sh
+++ deploy.sh
@@ -7,6 +7,6 @@ venv/bin/easy_install zc.buildout distribute
 # Setup the lxml egg before calling buildout (lxml has problems)
 mkdir ./eggs
 cp -r lxml-binaries/lxml ./eggs/
-cp lxml-binaries/linux/lxml-3.1.0-py2.7.egg-info ./eggs/
+cp lxml-binaries/lxml-3.1.0-py2.7.egg-info ./eggs/
 
 venv/bin/buildout
